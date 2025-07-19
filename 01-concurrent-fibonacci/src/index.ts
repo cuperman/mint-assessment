613ee2db7631e186b17d580ed420db5179fd7217
Jeff Cooper <jeff@cuperman.net>
2025-07-19 08:13:45 -0700

    Use cache fetch pattern to decouple caching logic

--- 01-concurrent-fibonacci/src/index.ts
+++ 01-concurrent-fibonacci/src/index.ts
@@ -1,22 +1,22 @@
+import { Cache } from "./cache";
+
 const DELAY_TIME = 5;
 
-const cache: { [key: number]: number } = {
-  [0]: 0,
-  [1]: 1,
-};
+const cache = new Cache<number, number>(
+  new Map<number, number>([
+    [0, 0],
+    [1, 1],
+  ])
+);
 
 export async function concurrentMemoFibonacci(n: number): Promise<number> {
-  if (typeof cache[n] !== "undefined") {
-    return cache[n];
-  }
-
-  return new Promise((resolve, _reject) => {
-    setTimeout(async () => {
-      const nMinus1 = await concurrentMemoFibonacci(n - 1);
-      const nMinus2 = await concurrentMemoFibonacci(n - 2);
-      const result = nMinus1 + nMinus2;
-      cache[n] = result;
-      resolve(result);
-    }, DELAY_TIME);
+  return cache.fetch(n, async () => {
+    return new Promise((resolve, _reject) => {
+      setTimeout(async () => {
+        const nMinus1 = await concurrentMemoFibonacci(n - 1);
+        const nMinus2 = await concurrentMemoFibonacci(n - 2);
+        resolve(nMinus1 + nMinus2);
+      }, DELAY_TIME);
+    });
   });
 }
